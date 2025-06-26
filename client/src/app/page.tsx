f1d855d57bad4566b9cb9447cbc852460f81144c
shivv <shivpratikhande2017@gmail.com>
2025-06-27 00:07:54 +0530

    updated new server to the seervjs file

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import GitHubWebhookManager from "@/components/github";
 import LandingPage from "@/components/landing";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <LandingPage/>
+      <GitHubWebhookManager/>
     </div>
 
   )}
